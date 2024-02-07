7a46913dd16c23200a228dc5cc8dd185e13a1d1f
Jerry <jerryjake227@gmail.com>
2024-02-07 19:13:00 +0100

    Update content-benefit.php

--- pathtg/assets/template-parts/misc/content-benefit.php
+++ pathtg/assets/template-parts/misc/content-benefit.php
@@ -1,1 +1,53 @@
+  <!-- benefit of working with us  -->
+  <div class="container-fluid pad-all benefit div-sections">
+    <div class="row gy-5 justify-content-center">
+      <div class="col-lg-12">
+        <h2> Benefits of working <br> with us</h2>
+      </div>
+      <div class="col-lg-6 d-flex">
+        <div class="img-div me-lg-5 me-2">
+        <img src="<?php echo get_template_directory_uri (); ?>/assets/images/receipt-edit.svg" alt="" class="">
+        </div>
+        <div class="div">
+          <h4> Friendly interest rates on loans</h4>
+          <p  class="text-black-50">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-6 d-flex">
+      <div class="img-div me-lg-5 me-2">
+        <img src="<?php echo get_template_directory_uri (); ?>/assets/images/receipt-edit.svg" alt="" class="">
+        </div>
+        <div class="div">
+          <h4>Full spectrum of support </h4>
+          <p class="text-black-50">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-6 d-flex">
+      <div class="img-div me-lg-5 me-2">
+        <img src="<?php echo get_template_directory_uri (); ?>/assets/images/receipt-edit.svg" alt="" class="">
+        </div>
+        <div class="div">
+          <h4>Installmental payment</h4>
+          <p  class="text-black-50">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-6 d-flex">
+      <div class="img-div me-lg-5 me-2">
+        <img src="<?php echo get_template_directory_uri (); ?>/assets/images/receipt-edit.svg" alt="" class="">
+        </div>
+        <div class="div">
+          <h4> Flexible work teams</h4>
+          <p  class="text-black-50">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 
